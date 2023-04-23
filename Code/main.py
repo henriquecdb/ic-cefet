370edd30aa5ab3e04590343fcac64fefde63662f
henriquecdb <henriquecamposj@gmail.com>
2023-04-23 15:10:41 -0300

    Run python code with args

--- Code/main.py
+++ Code/main.py
@@ -3,12 +3,16 @@
 from bugs.Bug import *
 from bugs.Robot import *
 import numpy as np
+import sys
+
+
+def main(x, y, th):
+    # bug = Robot('Pioneer_p3dx', 0.331, 0.09751, 1., np.deg2rad(45), False, 3, 3)
+    # bug.run()
+
+    newBug = Bug(x, y, th)
+    newBug._bug1()
+    # newBug._bug2()
 
 if __name__ == '__main__':
-
-    #bug = Robot('Pioneer_p3dx', 0.331, 0.09751, 1., np.deg2rad(45), False, 3, 3)
-    #bug.run()
-
-    newBug = Bug(4, 4, 90)
-    newBug._bug1()
-    #newBug._bug2()
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
